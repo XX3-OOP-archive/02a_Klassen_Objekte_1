4357f5a95941f24e1d5feaadccc415f87c6d754d
Peter <schmidt.multimedia@gmail.com>
2021-12-01 15:12:43 +0100

    new Cat()

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -4,6 +4,8 @@ public class App {
     
     public static void main(String[] args) {
 
+        Cat cat = new Cat(); // Was erzeugen wir hier eigentlich??
+        System.out.println(cat);
     
     }
 

7a9577457ca8ca562435508ac9d1e30f69eca460
Peter <schmidt.multimedia@gmail.com>
2021-12-02 11:11:16 +0100

    Wertzuweisung von aussen / innen

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -4,10 +4,31 @@ public class App {
     
     public static void main(String[] args) {
 
-        Cat cat = new Cat(); // Was erzeugen wir hier eigentlich??
-        System.out.println("Blick von aussen: " + cat);
+        Cat cat1 = new Cat(); // Klasse --> Objekt / Instanz
+        System.out.println("Blick von aussen: " + cat1);
+        cat1.name = "Grizabella";
+        cat1.furColor = "white";
+        cat1.age = 29;
+        cat1.tellYourAddress();
+        System.out.println(cat1.tellYourName());
+        System.out.println(cat1.furColor);
+        System.out.println(cat1.age);
+
+        System.out.println("------------------------");
+
+        Cat cat2 = new Cat(); // Klasse --> Objekt / Instanz
+        System.out.println("Blick von aussen: " + cat2);
+        cat2.name = "Alonzo";
+        cat2.furColor = "black";
+        cat2.age = 35;
+        cat2.tellYourAddress();
+        System.out.println(cat2.tellYourName());
+        System.out.println(cat2.furColor);
+        System.out.println(cat2.age);
+
+
+
         
-        cat.tellYourAddress();
 
     }
 
--- src/com/cc/java/Cat.java
+++ src/com/cc/java/Cat.java
@@ -2,8 +2,16 @@ package com.cc.java;
 
 public class Cat {
     
+    public String name;
+    public String furColor;
+    public int age;
+
     public void tellYourAddress(){
         System.out.println("Blick von innen: " + this);
     }
     
+    public String tellYourName(){
+       return this.name;
+    }
+
 }
